a13c84025b4ea37c6520907f3c0c1bae9a8a97ae
Rieu Joan <j.rieu@lectra.com>
2018-11-06 16:00:17 +0100

    remove all code

--- star-wars/src/extension.ts
+++ star-wars/src/extension.ts
@@ -1,79 +1,10 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode'
-import * as SWAPI from './SWAPI'
-
-async function showCharacters() {
-    const characters = await SWAPI.fetchCharacters()
-    const names = characters.map(c => c.name)
-    const characterName = await vscode.window.showQuickPick(names)
-    if (characterName)
-        vscode.window.showInformationMessage(characterName)
-}
-
-async function searchCharacters() {
-    const qp = vscode.window.createQuickPick()
-    qp.placeholder = "Start typing to search using the API"
-    qp.onDidChangeValue(async input => {
-        qp.busy = true
-        if (input) {
-            const characters = await SWAPI.searchCharactersByName(input)
-            if (qp.value !== input)
-                return
-            qp.items = characters.map(c => ({ label: c.name }))
-        } else {
-            qp.items = []
-        }
-        qp.busy = false
-    })
-    qp.show()
-}
-
-const swTreeProvider: vscode.TreeDataProvider<SWAPI.Character> = {
-    async getChildren(element?) {
-        if (element)
-            return []
-        return await SWAPI.fetchCharacters()
-    },
-    getTreeItem(character) {
-        const resourceUri = vscode.Uri.parse("sw://characters/" + character.name + "#" + character.url)
-        return {
-            resourceUri,
-            command: {
-                command: "markdown.showPreview",
-                title: "Show character",
-                arguments: [
-                    resourceUri
-                ]
-            }
-        }
-    }
-}
-
-const swContentProvider: vscode.TextDocumentContentProvider = {
-    async provideTextDocumentContent(uri, token) {
-        const character = await SWAPI.fetchCharacter(uri.fragment)
-        return `
-# ${character.name}
-
-${character.name} was born in the year ${character.birth_year}.
-
-![](${await SWAPI.fetchCharacterImageURL(character.name)})
-
-| Height                 | Mass                 |
-|------------------------|----------------------|
-| ${character.height} cm | ${character.mass} kg |
-`
-    }
-}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    vscode.commands.registerCommand("starWars.showCharacters", showCharacters)
-    vscode.commands.registerCommand("starWars.searchCharacters", searchCharacters)
-    vscode.window.registerTreeDataProvider("starWars.characters", swTreeProvider)
-    vscode.workspace.registerTextDocumentContentProvider("sw", swContentProvider)
 }
 
 // this method is called when your extension is deactivated
