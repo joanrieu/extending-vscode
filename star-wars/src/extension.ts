8059fc1ff13964d632358875b1a5a66b0823d5b1
Rieu Joan <j.rieu@lectra.com>
2019-04-17 12:16:20 +0200

    add SWAPI import

--- star-wars/src/extension.ts
+++ star-wars/src/extension.ts
@@ -1,5 +1,12 @@
 import * as vscode from "vscode";
+import * as SWAPI from "./SWAPI";
 
-export function activate(context: vscode.ExtensionContext) {}
+export function activate(context: vscode.ExtensionContext) {
+  vscode.commands.registerCommand("sw.pick", () => {
+    vscode.window.showQuickPick(
+      SWAPI.fetchCharacters().then(characters => characters.map(c => c.name))
+    );
+  });
+}
 
 export function deactivate() {}
